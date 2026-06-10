65f3bff5f7bb8b2364a2b927ce05b3649b3404eb
Darshan <drsofficial3k@gmail.com>
2026-06-10 23:18:19 +0530

    cleanup: remove debug endpoints

--- api/__delete.ts
+++
@@ -1,8 +0,0 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { db, schema } from '../../src/db/index.js';
-import { eq } from 'drizzle-orm';
-
-export default async function handler(req: VercelRequest, res: VercelResponse) {
-  await db.delete(schema.connectedAccounts).where(eq(schema.connectedAccounts.provider, 'instagram'));
-  res.json({ ok: true });
-}
